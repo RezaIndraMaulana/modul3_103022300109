5c84f183b5dec077d8b9877ef52c0c61217f1919
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-05 16:48:39 +0700

    sudah selesai ya ges ya

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,229 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Output = new System.Windows.Forms.Label();
+            this.angka1 = new System.Windows.Forms.Button();
+            this.angka2 = new System.Windows.Forms.Button();
+            this.angka3 = new System.Windows.Forms.Button();
+            this.angka4 = new System.Windows.Forms.Button();
+            this.angka5 = new System.Windows.Forms.Button();
+            this.angka6 = new System.Windows.Forms.Button();
+            this.angka7 = new System.Windows.Forms.Button();
+            this.angka8 = new System.Windows.Forms.Button();
+            this.angka9 = new System.Windows.Forms.Button();
+            this.tambah = new System.Windows.Forms.Button();
+            this.angka0 = new System.Windows.Forms.Button();
+            this.samadengan = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.hasilJumlah = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Output
+            // 
+            this.Output.AutoSize = true;
+            this.Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Output.Location = new System.Drawing.Point(365, 48);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(0, 25);
+            this.Output.TabIndex = 0;
+            this.Output.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Output.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // angka1
+            // 
+            this.angka1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.angka1.Location = new System.Drawing.Point(261, 137);
+            this.angka1.Name = "angka1";
+            this.angka1.Size = new System.Drawing.Size(107, 56);
+            this.angka1.TabIndex = 1;
+            this.angka1.Text = "1";
+            this.angka1.UseVisualStyleBackColor = true;
+            this.angka1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // angka2
+            // 
+            this.angka2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.angka2.Location = new System.Drawing.Point(374, 137);
+            this.angka2.Name = "angka2";
+            this.angka2.Size = new System.Drawing.Size(107, 56);
+            this.angka2.TabIndex = 2;
+            this.angka2.Text = "2";
+            this.angka2.UseVisualStyleBackColor = true;
+            this.angka2.Click += new System.EventHandler(this.angka2_Click);
+            // 
+            // angka3
+            // 
+            this.angka3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.angka3.Location = new System.Drawing.Point(487, 137);
+            this.angka3.Name = "angka3";
+            this.angka3.Size = new System.Drawing.Size(107, 56);
+            this.angka3.TabIndex = 3;
+            this.angka3.Text = "3";
+            this.angka3.UseVisualStyleBackColor = true;
+            this.angka3.Click += new System.EventHandler(this.angka3_Click);
+            // 
+            // angka4
+            // 
+            this.angka4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.angka4.Location = new System.Drawing.Point(261, 199);
+            this.angka4.Name = "angka4";
+            this.angka4.Size = new System.Drawing.Size(107, 56);
+            this.angka4.TabIndex = 4;
+            this.angka4.Text = "4";
+            this.angka4.UseVisualStyleBackColor = true;
+            this.angka4.Click += new System.EventHandler(this.angka4_Click);
+            // 
+            // angka5
+            // 
+            this.angka5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.angka5.Location = new System.Drawing.Point(374, 199);
+            this.angka5.Name = "angka5";
+            this.angka5.Size = new System.Drawing.Size(107, 56);
+            this.angka5.TabIndex = 5;
+            this.angka5.Text = "5";
+            this.angka5.UseVisualStyleBackColor = true;
+            this.angka5.Click += new System.EventHandler(this.angka5_Click);
+            // 
+            // angka6
+            // 
+            this.angka6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.angka6.Location = new System.Drawing.Point(487, 199);
+            this.angka6.Name = "angka6";
+            this.angka6.Size = new System.Drawing.Size(107, 56);
+            this.angka6.TabIndex = 6;
+            this.angka6.Text = "6";
+            this.angka6.UseVisualStyleBackColor = true;
+            this.angka6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // angka7
+            // 
+            this.angka7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.angka7.Location = new System.Drawing.Point(261, 261);
+            this.angka7.Name = "angka7";
+            this.angka7.Size = new System.Drawing.Size(107, 56);
+            this.angka7.TabIndex = 7;
+            this.angka7.Text = "7";
+            this.angka7.UseVisualStyleBackColor = true;
+            this.angka7.Click += new System.EventHandler(this.angka7_Click);
+            // 
+            // angka8
+            // 
+            this.angka8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.angka8.Location = new System.Drawing.Point(374, 261);
+            this.angka8.Name = "angka8";
+            this.angka8.Size = new System.Drawing.Size(107, 56);
+            this.angka8.TabIndex = 8;
+            this.angka8.Text = "8";
+            this.angka8.UseVisualStyleBackColor = true;
+            this.angka8.Click += new System.EventHandler(this.angka8_Click);
+            // 
+            // angka9
+            // 
+            this.angka9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.angka9.Location = new System.Drawing.Point(487, 261);
+            this.angka9.Name = "angka9";
+            this.angka9.Size = new System.Drawing.Size(107, 56);
+            this.angka9.TabIndex = 9;
+            this.angka9.Text = "9";
+            this.angka9.UseVisualStyleBackColor = true;
+            this.angka9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // tambah
+            // 
+            this.tambah.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tambah.Location = new System.Drawing.Point(261, 323);
+            this.tambah.Name = "tambah";
+            this.tambah.Size = new System.Drawing.Size(107, 56);
+            this.tambah.TabIndex = 10;
+            this.tambah.Text = "+";
+            this.tambah.UseVisualStyleBackColor = true;
+            this.tambah.Click += new System.EventHandler(this.tambah_Click);
+            // 
+            // angka0
+            // 
+            this.angka0.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.angka0.Location = new System.Drawing.Point(374, 323);
+            this.angka0.Name = "angka0";
+            this.angka0.Size = new System.Drawing.Size(107, 56);
+            this.angka0.TabIndex = 11;
+            this.angka0.Text = "0";
+            this.angka0.UseVisualStyleBackColor = true;
+            this.angka0.Click += new System.EventHandler(this.angka0_Click);
+            // 
+            // samadengan
+            // 
+            this.samadengan.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.samadengan.Location = new System.Drawing.Point(487, 323);
+            this.samadengan.Name = "samadengan";
+            this.samadengan.Size = new System.Drawing.Size(107, 56);
+            this.samadengan.TabIndex = 12;
+            this.samadengan.Text = "=";
+            this.samadengan.UseVisualStyleBackColor = true;
+            this.samadengan.Click += new System.EventHandler(this.samadengan_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(390, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 13;
+            // 
+            // hasilJumlah
+            // 
+            this.hasilJumlah.AutoSize = true;
+            this.hasilJumlah.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hasilJumlah.Location = new System.Drawing.Point(396, 53);
+            this.hasilJumlah.Name = "hasilJumlah";
+            this.hasilJumlah.Size = new System.Drawing.Size(70, 25);
+            this.hasilJumlah.TabIndex = 14;
+            this.hasilJumlah.Text = "label2";
+            this.hasilJumlah.Click += new System.EventHandler(this.hasilJumlah_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.hasilJumlah);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.samadengan);
+            this.Controls.Add(this.angka0);
+            this.Controls.Add(this.tambah);
+            this.Controls.Add(this.angka9);
+            this.Controls.Add(this.angka8);
+            this.Controls.Add(this.angka7);
+            this.Controls.Add(this.angka6);
+            this.Controls.Add(this.angka5);
+            this.Controls.Add(this.angka4);
+            this.Controls.Add(this.angka3);
+            this.Controls.Add(this.angka2);
+            this.Controls.Add(this.angka1);
+            this.Controls.Add(this.Output);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Output;
+        private System.Windows.Forms.Button angka1;
+        private System.Windows.Forms.Button angka2;
+        private System.Windows.Forms.Button angka3;
+        private System.Windows.Forms.Button angka4;
+        private System.Windows.Forms.Button angka5;
+        private System.Windows.Forms.Button angka6;
+        private System.Windows.Forms.Button angka7;
+        private System.Windows.Forms.Button angka8;
+        private System.Windows.Forms.Button angka9;
+        private System.Windows.Forms.Button tambah;
+        private System.Windows.Forms.Button angka0;
+        private System.Windows.Forms.Button samadengan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label hasilJumlah;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,158 @@ namespace modul3_103022300109
 {
     public partial class Form1 : Form
     {
+        int jumlah1, jumlah2, hasil;
+        bool tambahTotal = false;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!tambahTotal)
+            {
+                jumlah1 =  jumlah1 * 10 + 1;
+            }
+            else
+            {
+                jumlah2 = jumlah2 * 10 + 1;
+            }
+        }
+
+        private void angka2_Click(object sender, EventArgs e)
+        {
+            if (!tambahTotal)
+            {
+                jumlah1 = jumlah1 * 10 + 2;
+            }
+            else
+            {
+                jumlah2 = jumlah2 * 10 + 2;
+            }
+        }
+
+        private void angka3_Click(object sender, EventArgs e)
+        {
+            if (!tambahTotal)
+            {
+                jumlah1 = jumlah1 * 10 + 3;
+            }
+            else
+            {
+                jumlah2 = jumlah2 * 10 + 3;
+            }
+        }
+
+        private void angka4_Click(object sender, EventArgs e)
+        {
+            if (!tambahTotal)
+            {
+                jumlah1 = jumlah1 * 10 + 4;
+            }
+            else
+            {
+                jumlah2 = jumlah2 * 10 + 4;
+            }
+        }
+
+        private void angka5_Click(object sender, EventArgs e)
+        {
+            if (!tambahTotal)
+            {
+                jumlah1 = jumlah1 * 10 + 5;
+            }
+            else
+            {
+                jumlah2 = jumlah2 * 10 + 5;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (!tambahTotal)
+            {
+                jumlah1 = jumlah1 * 10 + 6;
+            }
+            else
+            {
+                jumlah2 = jumlah2 * 10 + 6;
+            }
+        }
+
+        private void angka7_Click(object sender, EventArgs e)
+        {
+            if (!tambahTotal)
+            {
+                jumlah1 = jumlah1 * 10 + 7;
+            }
+            else
+            {
+                jumlah2 = jumlah2 * 10 + 7;
+            }
+        }
+
+        private void angka8_Click(object sender, EventArgs e)
+        {
+            if (!tambahTotal)
+            {
+                jumlah1 = jumlah1 * 10 + 8;
+            }
+            else
+            {
+                jumlah2 = jumlah2 * 10 + 8;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (!tambahTotal)
+            {
+                jumlah1 = jumlah1 * 10 + 9;
+            }
+            else
+            {
+                jumlah2 = jumlah2 * 10 + 9;
+            }
+        }
+
+        private void tambah_Click(object sender, EventArgs e)
+        {
+            tambahTotal = true;
+            hasilJumlah.Text = jumlah1 + " + " + jumlah2;
+        }
+
+        private void angka0_Click(object sender, EventArgs e)
+        {
+            if (!tambahTotal)
+            {
+                jumlah1 = jumlah1 * 10 + 0;
+            }
+            else
+            {
+                jumlah2 = jumlah2 * 10 + 0;
+            }
+        }
+
+        private void hasilJumlah_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void samadengan_Click(object sender, EventArgs e)
+        {
+            hasil = jumlah1 + jumlah2;
+
+            hasilJumlah.Text = "Hasil = " + hasil;
+            jumlah1 = hasil;
+            jumlah2 = 0;
         }
     }
 }
